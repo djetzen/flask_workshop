fefd320bc1a98560a22d864dc80fdc1d60258862
Dominik Jetzen <jetzen@itemis.de>
2019-06-13 12:08:11 +0200

    add simple get and post service

---
+++ first/first.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/hello")
+def hello():
+    return "Hello Flask!"
+
+
+@app.route("/hello/post", methods=["POST"])
+def hello_post():
+    return b"The data sent is: "+request.data
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
